197ef2aa245ade90952dc2b479cf8741ad769924
Jgouken <jjaidenj09@gmail.com>
2021-07-01 16:22:21 -0700

    Discord Update v13

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const bot = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_PRESENCES', 'GUILD_MEMBERS'] });
 
 module.exports = {
     Discord: Discord, 
