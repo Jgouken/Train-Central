a09d6af6ab6cf2105c4f95852c5b08e7198b6ba4
Jgouken <jjaidenj09@gmail.com>
2021-07-01 17:21:47 -0700

    ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const config = require('./config/config')
 const {bot, Discord} = require('./config/config')
 const guild = bot.guilds.cache.get('851966158651392040')
 const train = bot.channels.cache.get('860023491729817620')
+const my = bot.members.cache.get('860171173518245928')
 
 let regions = [
   "Work Region",
@@ -14,7 +15,7 @@ var currentRegion = regions[0]
 
 async function getNextRegion() {
   for (let i = -1; i < regions.length; i++) {
-    if (guild.me.roles.find(regions[i])) {
+    if (my.roles.find(regions[i])) {
       if (i >= regions.length) return regions[0]
       else return regions[(i + 1)]
     } else if (i >= regions.length) return regions[0]
@@ -29,13 +30,13 @@ bot.on('ready', async () => {
     const role = guild.roles.cache.find(role => role.name === afterRegion)
 
     // Move the train
-    if (guild.me.roles.cache.get(role => role.name === beforeRegion)) guild.me.roles.remove((guild.me.roles.cache.get(role => role.name === beforeRegion)).id).catch(() => {return})
+    if (my.roles.cache.get(role => role.name === beforeRegion)) my.roles.remove((my.roles.cache.get(role => role.name === beforeRegion)).id).catch(() => {return})
 
-    guild.me.roles.add(role)
+    my.roles.add(role)
     
     currentRegion = afterRegion
     train.send(`I'm now at the ${currentRegion}!`)
-  }, 60 * 1000) // 120 seconds
+  }, 20 * 1000) // 120 seconds
 })
 
 bot.login(config.TOKEN)
