ed6f4b8f9e2515208e02e146a637b2b6c0452ad5
Jgouken <jjaidenj09@gmail.com>
2021-07-01 22:34:11 -0700

    Get on the train

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require('./config/config')
 const {bot, Discord} = require('./config/config')
+const riders = new Set();
 
 let regions = [
   "Work Region",
@@ -24,6 +25,7 @@ bot.on('ready', async () => {
   const guild = bot.guilds.cache.get("851966158651392040")
   const train = bot.channels.cache.get("860023491729817620")
   const member = guild.members.cache.get("860171173518245928")
+  for (let i = -1; i < regions.length; i++) member.roles.remove(regionIDs[i]).catch(() => {return})
 
     var interval = setInterval (async function () {
       train.send('**15 SECONDS BEFORE THE TRAIN LEAVES.**')
@@ -32,8 +34,32 @@ bot.on('ready', async () => {
         member.roles.remove(regionIDs[currentRegion])
         currentRegion += 1
         if (!regions[currentRegion]) currentRegion = 0
-        
         member.roles.add(regionIDs[currentRegion])
+
+        riders.forEach(user => {
+          let member = guild.members.cache.get(user)
+          for (let i = -1; i < regions.length; i++) member.roles.remove(regionIDs[i]).catch(() => {return})
+          member.roles.add(regionIDs[currentRegion])
+        })
+
+        riders.clear()
+
+        train.send(`I am now in the ${regions[currentRegion]}! ALL ABOOOOOOARD! (React with 🚉 to join the ride)!`)
+        .then((m) => {
+          const filter = (reaction, user) => {
+            return reaction.emoji.name === '🚉' && !user.bot
+          };
+          
+          const collector = m.createReactionCollector(filter, { time: 120 * 1000 });
+          
+          collector.on('collect', (reaction, user) => {
+            riders.add(user.id)
+          });
+
+          collector.on('end', () => {
+            m.reactions.removeAll()
+          })
+        })
       }, 15 * 1000);
 
     }, 105 * 1000);
